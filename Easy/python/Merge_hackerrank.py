8bbb6b281a9f8b27d3a488a1a97908e0081ca883
lawrence <lawrence2000april@gmail.com>
2024-11-30 19:17:20 +0530

    hacker

--- Easy/python/Merge_hackerrank.py
+++ Easy/python/Merge_hackerrank.py
@@ -1,7 +1,17 @@
 def merge_the_tools(string,k):
-    
+    t = []
     for i in range(0,len(string),k):
+        #print(string[i])
+        t.append(string[i:i+k])
         s = string[i:i+k]
+        # for r in range(len(s)):
+        #     if s[r] in [h for h in s]:
+        [h for h in s]
+
+
+    for j in t:
+        print(j)
+        
         
 
 
